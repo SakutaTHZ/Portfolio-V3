882135e2e6ac1defee19072ad4c1b9f1d73ab4c7
Thar Htet Zan <thzarts@gmail.com>
2024-10-30 09:18:46 +0630

    Added fancy gradient border to the glassboxes

--- src/components/RollText.jsx
+++ src/components/RollText.jsx
@@ -1,20 +1,27 @@
-const RollText = ({ customClass, text }) => {
-    let letters = text.split("");
-  
-    return (
-      <div className={`textContainer ${customClass}`}>
-        {letters.map((letter, index) => (
-          <span
-            key={index}
-            className="letter"
-            data-letter={letter}
-            style={{ transitionDelay: `${index * 0.05}s` }} // Set transition delay inline
-          >
-            {letter}
-          </span>
-        ))}
-      </div>
-    );
-  };
-  
-  export default RollText;
+const RollText = ({ customClass, text, gradient }) => {
+  let letters = text.split("");
+  let gradientColor = gradient
+    ? `after:text-[var(--primary-color)] before:text-[var(--primary-color)]`
+    : `after:text-[var(--text-color)] before:text-[var(--text-color)]`;
+
+  return (
+    <div className={`textContainer ${customClass}`}>
+      {letters.map((letter, index) => (
+        <span
+          key={index}
+          className={`letter ${gradientColor}`}
+          data-letter={letter}
+          style={{
+            "--hue-rotate": `${index * 10}deg`,
+            filter: "hue-rotate(var(--hue-rotate))",
+            transitionDelay: `${index * 0.05}s`,
+          }}
+        >
+          {letter}
+        </span>
+      ))}
+    </div>
+  );
+};
+
+export default RollText;
--- src/layout/HomeSection.jsx
+++ src/layout/HomeSection.jsx
@@ -5,16 +5,24 @@ const HomeSection = () => {
     <div className="h-screen w-screen px-64 py-20">
       <div className="grid h-full grid-cols-5 grid-rows-5 gap-4">
         <div className="glassBox flex items-end col-span-2 row-span-3">
-            <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-4">
             <p className="text-base">Hi! My name is</p>
-            <RollText customClass={`text-5xl font-bold`} text={'Thar Htet Zan'}/>
-            </div>
+            <RollText
+              customClass={`text-5xl font-bold `}
+              gradient={false}
+              text={"Thar Htet Zan"}
+            />
+            <p className="text-sm">
+              Hey there! I'm a software developer 💻 with a big passion for
+              UI/UX and front-end development. I absolutely love bringing
+              creative concepts and fresh ideas to life on the screen. 😊 Let's
+              make something amazing together!
+            </p>
+          </div>
         </div>
         <div className="glassBox col-span-2 col-start-1 row-start-4"></div>
         <div className="glassBox col-span-2 col-start-1 row-start-5"></div>
-        <div className="glassBox col-span-2 row-span-5 col-start-3 row-start-1">
-          
-        </div>
+        <div className="glassBox col-span-2 row-span-5 col-start-3 row-start-1"></div>
         <div className="glassBox row-span-2 col-start-5 row-start-1"></div>
         <div className="glassBox row-span-3 col-start-5 row-start-3"></div>
       </div>
